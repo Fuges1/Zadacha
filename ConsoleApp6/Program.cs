6a8a4c214c9f29f2a4a1995a4025442df1af0752
telaz <telaz@DESKTOP-N7DCKG9>
2023-05-31 13:47:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Data1
+{
+    class score
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i <= 987; i++)
+            {
+                string Stri = i.ToString();
+                if (Stri[0] != Stri[1] && Stri[1] != Stri[2] && Stri[0] != Stri[2])
+                {
+                    Console.Write("{0,4}", i);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Нажмите любую кнопку чтобы закрыть программу");
+           
+        }
+    }
+
+}
